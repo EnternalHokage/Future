13eb32e8c9de1c5d42107cb9d0ce20f42c2188d8
EnternalHokage <43544072+EnternalHokage@users.noreply.github.com>
2019-03-20 10:04:32 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,4 @@
+$("button").click(function(){
+var name= $(".ageInput").val();
+$("p").text("Hello you're gonna be 25.There's gonna be superpowers in the future and your power will be able to teleport");
+});
